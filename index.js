04e507a6026e4a25b81b93f4dc0dc0b32b2ebe6d
A Hoerman <awhoerman@me.com>
2020-03-01 17:25:33 -0500

    added prompt & axios request

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
-const fs = require("fs");
+// const fs = require("fs");
 
-const generator = require("./generator");
+// const generator = require("./generator");
 
+inquirer
+    .prompt({
+        type: "input",
+        message: "GitHub username?",
+        name: "name"
+    }).then(function({name}){
+        const queryURL = `https://api.github.com/users/${name}`;
+
+        axios.get(queryURL).then(function(response){
+            console.log(response);
+        });
+
+    });

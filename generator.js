709a3b0bae693600503970c986a011194f0ff32e
A Hoerman <awhoerman@me.com>
2020-03-01 15:55:30 -0500

    added generator.js, package.json

---
+++ generator.js
@@ -0,0 +1,6 @@
+const generateReadMe = (response) => {
+
+
+}
+
+module.exports = generateReadMe;
---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+
+const generator = require("./generator");
+

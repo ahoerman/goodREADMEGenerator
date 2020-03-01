4100dffa805d676c464cd9b32404dc4adf2d86b8
A Hoerman <awhoerman@me.com>
2020-03-01 17:05:49 -0500

    generator

--- generator.js
+++ generator.js
@@ -1,6 +1,37 @@
 const generateReadMe = (response) => {
 
 
+let newReadMe = `![last commit](https://img.shields.io/badge/Updated-${dateoflastupdate}-brightgreen)
+    # ${title}
+    ${description}
+    ## Table of Contents
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Licensing](#Licensing)
+    - [Contribution](#Contribution)
+    - [Testing](#Testing)
+    - [Questions](#Questions)
+    ## Installation 
+    ${installationInstruc}
+    ## Usage
+    ${usage}
+    ## Licensing
+    ${license}
+    ## Contribution
+    ${contributors}
+    ## Testing
+    ${testing}
+    ## Questions
+    Email: ${email}
+    ![alt txt](${bioimg})`
+
+fs.writeFile("README.txt", newReadMe, function(err){
+    if (err) {
+        throw err;
+    } else {
+        console.log("ReadMe successfully created!");
+    }
+});
 }
 
 module.exports = generateReadMe;

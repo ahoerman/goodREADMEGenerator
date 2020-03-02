f8ecf19995992ae82ff602e9383b15f393f8f66c
A Hoerman <awhoerman@me.com>
2020-03-01 20:56:01 -0500

    added all questions, linked generator

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
-// const fs = require("fs");
+const fs = require("fs");
 
-// const generator = require("./generator");
+const generator = require("./generator");
 
 inquirer
-    .prompt({
+    .prompt([{
         type: "input",
-        message: "GitHub username?",
+        message: "GitHub Username?",
         name: "name"
-    }).then(function({name}){
-        const queryURL = `https://api.github.com/users/${name}`;
+    },{
+        type: "input",
+        message: "Project Title?",
+        name: "title"
+    },{
+        type: "input",
+        message: "Project Description?",
+        name: "description"
+    },{
+        type: "input",
+        message: "Installation Instructions?",
+        name: "install"
+    },{
+        type: "input",
+        message: "Project Usage?",
+        name: "usage"
+    },{
+        type: "list",
+        choices: ["GNU AGPLv3", "Mozilla Public License 2.0", "Apache License 2.0","MIT License", "No License"],
+        message: "Any Licensing?",
+        name: "license"
+    },{
+        type: "input",
+        message: "Project Contributors?",
+        name: "contributors"
+    },{
+        type: "input",
+        message: "Testing?",
+        name: "testing"
+    },{
+        type: "input",
+        message: "Questions?",
+        name: "questions"
+    },{
+        type: "input",
+        message: "Email?",
+        name: "email"
+    }]).then(function(response){
+        const queryURL = `https://api.github.com/users/${response.name}`;
+        let inqResponse = response;
 
-        axios.get(queryURL).then(function(response){
-            console.log(response);
+        axios.get(queryURL).then(function(data){
+            let lastUpdate = data.data.updated_at
+            let bioImg = data.data.avatar_url
+            let newReadMe = generator(inqResponse, lastUpdate, bioImg);
+            fs.writeFile("README.md", newReadMe, function(err){
+                if (err) {
+                    throw err;
+                } else {
+                    console.log("ReadMe successfully created!");
+                }
+            });
         });
+
 
     });

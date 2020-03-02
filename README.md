47b4d3ff51a55443538dd001930e57123d81955b
A Hoerman <awhoerman@me.com>
2020-03-01 21:54:09 -0500

    fixing ReadMe Markdown

--- README.md
+++ README.md
@@ -1,22 +1,44 @@
-# goodREADMEGenerator
-![Followers](https://img.shields.io/badge/Followers-3-brightgreen)
-    ![Location](https://img.shields.io/badge/Location-Portsmouth-blue)
-    <h1>Title</h1>
-   <br>
-    Description
-    <br>
-    <h2>Table of Contents</h2>
-    [Installation](#Installation)<br>
-    [Usage](#Usage)<br>
-    [Licensing](#Licensing)<br>
-    [Contributors](#Contributors)<br>
-    [Testing](#Testing)<br>
-    [Questions](#Questions)<br>
-    <h2>Installation</h2>
-    <h2>Usage</h2>
-    <h2>Licensing</h2>
-    <h2>Contributors</h2>
-    <h2>Testing</h2>
-    <h2>Questions</h2>
-    <h3>Email:</h3>
-    <img src="https://avatars3.githubusercontent.com/u/57423419?v=4">
+# README Generator
+
+![followers](https://img.shields.io/badge/Followers-3-brightgreen)
+![location](https://img.shields.io/badge/Location-Portsmouth-blue)
+
+## Table of Contents
+[Installation](#Installation)
+
+[Usage](#Usage)
+
+[Licensing](#Licensing)
+
+[Contributors](#Contributors)
+
+[Testing](#Testing)
+
+[Questions](#Questions)
+
+***
+
+## Installation
+
+
+## Usage
+
+
+## Licensing
+
+
+## Contributors
+
+
+## Testing
+
+
+## Questions
+
+
+***
+
+### Email:
+
+![bioImg](https://avatars3.githubusercontent.com/u/57423419?v=4)
+
--- generator.js
+++ generator.js
@@ -1,34 +1,29 @@
 const fs = require("fs");
 
 const generateReadMe = (response, followers, location, bioImage) => {
-    return `# ${response.title}
-    ![Followers](https://img.shields.io/badge/Followers-${followers}-brightgreen)
-        ![Location](https://img.shields.io/badge/Location-${location}-blue)
-        <h1>${response.title}</h1>
-       <br>
-        ${response.description}
-        <br>
-        <h2>Table of Contents</h2>
-        [Installation](#Installation)<br>
-        [Usage](#Usage)<br>
-        [Licensing](#Licensing)<br>
-        [Contributors](#Contributors)<br>
-        [Testing](#Testing)<br>
-        [Questions](#Questions)<br>
-        <h2>Installation</h2>
-        ${response.install}
-        <h2>Usage</h2>
-        ${response.usage}
-        <h2>Licensing</h2>
-        ${response.license}
-        <h2>Contributors</h2>
-        ${response.contributors}
-        <h2>Testing</h2>
-        ${response.testing}
-        <h2>Questions</h2>
-        ${response.questions}
-        <h3>Email: ${response.email}</h3>
-        <img src="${bioImage}">`
+    return `![followers](https://img.shields.io/badge/Followers-${followers}-brightgreen)
+    # ${response.title}
+    ${response.description}
+    ## Table of Contents
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Licensing](#Licensing)
+    - [Contribution](#Contribution)
+    - [Testing](#Testing)
+    - [Questions](#Questions)
+    ## Installation 
+    ${response.install}
+    ## Usage
+    ${response.usage}
+    ## Licensing
+    ${response.license}
+    ## Contribution
+    ${response.contributors}
+    ## Testing
+    ${response.testing}
+    ## Questions
+    Email: ${response.email}
+    ![alt txt](${bioImage})`
 }
 
 module.exports = generateReadMe;
